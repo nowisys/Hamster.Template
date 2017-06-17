dc6b37a4b802d326fc41a486671155c8616bc9ed
Frank Gabriel <frank.gabriel@tu-dresden.de>
2017-06-17 10:44:19 +0200

    Project template

---
+++ src/Hamster.Template/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Hamster.Template
+{
+    public class Class1
+    {
+    }
+}
---
+++ test/Hamster.Template.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Hamster.Template.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
